2c97e7b8053b0b7e49638777f68abae29151b68b
fahri-r <00.fahri.r@gmail.com>
2022-07-21 16:33:58 +0700

    fixing profile bug

--- pages/profile.js
+++ pages/profile.js
@@ -78,7 +78,7 @@ export default function Profile({ profile }) {
   return (
     <PageContainer isFixedNav>
       <Toaster position="top-center" reverseOrder={true} />
-      <Nav role={profile.user.role} avatar={account.avatar} />
+      <Nav role={profile.user.role} avatar={profile.avatar} />
       <PageContent centerContent={true}>
         <Container maxW="container.sm">
           <Heading marginBottom="1.5rem">Edit profile</Heading>
